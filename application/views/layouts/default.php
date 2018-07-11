269b4b2b4d4440921a78971db28a6c003da93f9c
Andres Aguilar <andresyoshimar@gmail.com>
2018-07-11 17:39:18 -0500

    Comenzando con el diseño de la plantilla

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -7,10 +7,13 @@
     <link rel="stylesheet" href="<?php echo base_url('assets/css/main.css') ?>">
 	</head>
 	<body>
-		<?php
-      /* Aqui se mete el codigo de la nueva vista */
-      echo $content_for_layout;
-    ?>
+		<div class='container'>
+			<div class='header'></div>
+			<div class='menu'></div>
+			<div class='content'>
+				<?php echo $content_for_layout; ?>
+			</div>
+		</div>
 		<!-- Import Vue.js -->
 	  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script> <!-- Develop version -->
 	  <!-- <script src="https://unpkg.com/vue@2.5.13/dist/vue.min.js"></script> -->
